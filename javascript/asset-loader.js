5b06ba7027f38b69366ea1543443449c5331809e
PartyProNL <partypronl@gmail.com>
2022-05-17 12:10:15 +0200

    Default game loop aan werken

--- javascript/asset-loader.js
+++ javascript/asset-loader.js
@@ -10,33 +10,11 @@ function preload() {
 
   // Laden van alle foto's
   this.load.image("ground", "assets/environment/ground.png");
+  this.load.image("background", "assets/environment/background.png");
   this.load.image("dino", "assets/character/pixil-frame-1.png");
+  this.load.image("cactus", "assets/encounters/cactus.png");
 }
 
 function create() {
   this.scene.launch('menu');
 }
-
-// class AssetLoader {
-//   assetsLocation;
-//   main;
-//
-//   assetsToLoad = ["ground.png", "cactus.png", "dinosaur.png"];
-//
-//   constructor(assetsLocation, main) {
-//     this.assetsLocation = assetsLocation;
-//     this.main = main;
-//   }
-//
-//   loadImage(name) {
-//     this.main.load.image(name, this.assetsLocation+"/"+name);
-//   }
-//
-//   loadAssets() {
-//     console.log("Loading assets...");
-//
-//     for(let i = 0; i < this.assetsToLoad.length; i++) {
-//       this.loadImage(this.assetsToLoad[i]);
-//     }
-//   }
-// }
