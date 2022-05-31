0814b01fa72bad1a85d5749d958db9c1cf822548
PartyProNL <partypronl@gmail.com>
2022-05-31 11:50:19 +0200

    Ninja skin bugfix

--- javascript/game.js
+++ javascript/game.js
@@ -227,7 +227,11 @@ function updateSecond() {
     laserTimeLeft--;
     cactusses.clear(true, true);
     if(laserTimeLeft <= 0) {
-      player.play('dino-run');
+      if(ninjaHitsLeft <= 0) {
+        player.play('dino-run');
+      } else {
+        player.play('ninja-run');
+      }
     }
   }
 }
